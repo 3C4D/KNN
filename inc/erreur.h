4a47ebfb499a5767e00d9c413599c84aa5c2173e
SchizoBoy <cadonienzo@gmail.com>
2021-04-18 11:13:35 +0200

    ajout de la fonction err_log changement des fonctions verif_nom_fic et chargement_fichier et mise en place de commentaires dans les fichiers erreur.c et erreur.h

--- inc/erreur.h
+++ inc/erreur.h
@@ -1,7 +1,13 @@
 #ifndef ERREUR_H_
 #define ERREUR_H_
 
+/* Renvoie un message d'erreur sur stderr */
+void err_log(char *error);
+
+/* Renvoie un message d'erreur sur stderr et interrompt le programme */
 void erreur(char *error);
+
+/* Vérifie une allocation (jamais utilisée) */
 void verif_alloc(void *ptr);
 
 #endif
--- src/erreur.c
+++ src/erreur.c
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* Renvoie un message d'erreur sur stderr */
+void err_log(char *error){
+  fprintf(stderr, "Erreur, %s", error);
+}
+
+/* Renvoie un message d'erreur sur stderr et interrompt le programme */
 void erreur(char *error){
   perror(error);
   exit(-1);
 }
 
+/* Vérifie une allocation (jamais utilisée) */
 void verif_alloc(void *ptr){
   if (ptr == NULL) {
     erreur("Erreur: Échec de l'allocation");
